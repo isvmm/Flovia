a613d3dbcfd3d7f3f5d740506dd156bf9968b2e5
Shivamsc07 <chyshivam303@gmail.com>
2026-04-16 17:04:51 +0530

    Completely remove middleware import to unblock build

--- apps/web/middleware.js
+++ apps/web/middleware.js
@@ -1,4 +1,4 @@
-import { updateSession } from "@/lib/supabase/middleware";
+import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   // Disabling middleware temporarily to unblock Vercel deployment
